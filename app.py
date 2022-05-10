b30f34e944713d43d1ea508e2fa4b5c110394d6c
Rana Karmakar <77492330+Ranakarmakar@users.noreply.github.com>
2022-05-10 19:06:37 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -6,7 +6,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def welcome():
-    return render_template("main.html")
+    return render_template("main.html", back_file='..models/NeilArmstrong.glb')
+
+
+arr = os.listdir('./flask_upload')
 
 
 @app.route('/model', methods=['GET', 'POST'])
@@ -15,12 +18,11 @@ def model():
         file = request.files["uploaded_file"]
         model_path = "./flask_upload/" + file.filename
         file.save(model_path)
-        arr = os.listdir('./flask_upload')
-        x = arr[0]
-        y = '../flask_upload/'
-        z = y + x
+        z = '../flask_upload/' + arr[0]
+        print(z)
     else:
         z = '..models/NeilArmstrong.glb'
+        print(z)
 
     return render_template("main.html", back_file=z)
 
